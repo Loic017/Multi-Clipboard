ceac9733bea6dc65736d44dab567e8b3c71fd597
C21066192 <l.loic017@gmail.com>
2022-07-02 21:45:58 +0100

    Save Function + Json File
    - Clipboard can now be saved with a corresponding key.
    - Creates json file called "clipboard.json.

--- MultiClipboard.py
+++ MultiClipboard.py
@@ -2,23 +2,32 @@ import sys
 import json
 import clipboard
 
-def save_items(filepath, data):
+SAVED_DATA = "clipboard.json"
+
+def save_data(filepath, data):
     with open(filepath, "w") as f:
         json.dump(data, f)
 
-def load_items(filepath):
-    with open(filepath, "r") as f:
-        data = json.load(f)
-        return data
-    
+def load_data(filepath):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
 
 if len(sys.argv) == 2:
+    data = load_data(SAVED_DATA)
     command = sys.argv[1]
     print(command)
     
     if command == "save":
-        pass
+        key = input("Enter Key >> ")
+        data[key] = clipboard.paste()
+        save_data(SAVED_DATA, data)
     elif command == "load":
+        #key = input("Enter Key")
+        #clipboard.copy(key)
         pass
     elif command == "list":
         pass
